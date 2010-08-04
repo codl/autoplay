9dd788aff8d5498ddc1c8114a30d727792a7c078
Corentin Delcourt <codl@aquageek.net>
2010-08-04 03:14:11 +0200

    add config variables to rc.d

--- rc.d
+++ rc.d
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-# general config
+user="codl"
+bin="/home/codl/bin/autoplay.py"
+
 . /etc/rc.conf
 . /etc/rc.d/functions
 
 case "$1" in
     start)
 	stat_busy "Starting Autoplay"
-  su codl -c "/home/codl/bin/autoplay" > /dev/null &
+  su $user -c "$bin" > /dev/null &
   stat_done
         ;;
     stop)
 	stat_busy "Stopping Autoplay"
-  for i in $(ps -Ao pid,args | grep autoplay | grep python | cut -d' ' -f1); do kill $i; done
+  for i in $(ps -Ao pid,args | grep $bin | grep python | cut -d' ' -f1); do kill $i; done
 	stat_done
 	;;
     restart)
