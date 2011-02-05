23b380a6030af085a40c2a7dc30b1c4df22720e2
Corentin Delcourt <codl@aquageek.net>
2011-02-05 23:46:07 +0100

    Fixes unicode problems (hopefully...)

--- autoplay.py
+++ autoplay.py
@@ -13,6 +13,7 @@ import random
 import sqlite3
 import time
 import io
+import sys
 from socket import error as socketerror
 
 ## Config
@@ -35,12 +36,14 @@ debug = False
 logfile = "/tmp/autoplay.log"
 ## /Config
 
+enc = sys.getfilesystemencoding()
+
 ## Functions
 def log(msg, stdout=False):
   """Logs to file, and optionally to stdout. Obvious enough"""
   if stdout or debug:
-    print str(msg)
-  logio.write(unicode(msg)+"\n")
+    print msg
+  logio.write(unicode(msg, enc)+"\n")
 
 def reconnect(i=1):
   if i == tries:
@@ -54,6 +57,7 @@ def reconnect(i=1):
 
 
 def addsong():
+  """Adds a semi-random song to the playlist"""
   rand = random.uniform(-0.1, 1.5)
   cursor.execute("select * from songs where karma>? and time < ?",
     (rand, int(time.time()-(60*(flood_delay-trigger*3)))))
@@ -68,8 +72,8 @@ def addsong():
       (songdata[2]+1, newkarma, int(time.time()), songdata[0],)
     )
     db.commit()
-    log("Adding song "+songdata[0]+" - Karma = "+
-      str(songdata[3])+" - Karma limit = "+str(rand))
+    log(("Adding song "+songdata[0]+" - Karma = "+
+      str(songdata[3])+" - Karma limit = "+str(rand)).encode(enc))
     client.add(songdata[0])
 
 def getsong(songfile):
@@ -83,6 +87,7 @@ def getsong(songfile):
   return data
 
 def karma(songdata, which=0):
+  """Returns karma for a song"""
   listened = float(songdata[1])
   added = float(songdata[2])
 
@@ -103,8 +108,8 @@ def listened(songdata):
     "update songs set listened=?, karma=?, time=? where file=?",
     (songdata[1]+1, newkarma, int(time.time()), songdata[0])
   )
-  log("Listened to "+songdata[0]+" - Karma = "+
-    str(newkarma)+" - Listens = "+str(songdata[1]+1))
+  log(("Listened to "+songdata[0]+" - Karma = "+
+    str(newkarma)+" - Listens = "+str(songdata[1]+1)).encode(enc))
   db.commit()
 ## /Functions
 
@@ -115,7 +120,7 @@ random.seed()
 client = mpd.MPDClient()
 
 logio = io.open(logfile, "at", buffering=1)
-logio.write(unicode("\n\nStarted "+str(time.time())+"\n"))
+log("\n\nStarted "+str(time.time())+"\n")
 
 log("Connecting...")
 try:
@@ -142,7 +147,7 @@ for song in stale:
   cursor.execute("delete from songs where file=?", (song,))
 db.commit()
 for song in client.list("file"):
-  nothing = getsong(unicode(song, "utf-8"))
+  nothing = getsong(unicode(song, enc))
 db.commit()
 log("OK! :)")
 armed = 1
@@ -165,7 +170,7 @@ while __name__ == "__main__":
     end = int(times[1])
     if armed == 1 and (end > mintime) and (pos > playtime*end/100):
       armed = 0 # Disarm until the next song
-      listened(getsong(unicode(client.currentsong()["file"], "utf-8")))
+      listened(getsong(unicode(client.currentsong()["file"], enc)))
       songid = (client.currentsong()["id"])
 
     if armed == 0 and not songid == client.currentsong()["id"]: 
