d904026cedd4064228f2c654233c479927be2317
Corentin Delcourt <codl@aquageek.net>
2011-02-06 14:31:34 +0100

    better background updating

--- autoplay.py
+++ autoplay.py
@@ -59,11 +59,12 @@ def reconnect(i=1):
 
 def addsong():
   """Adds a semi-random song to the playlist"""
-  rand = random.uniform(-0.1, 1.5)
+  rand = random.uniform(-0.5, 2)
   cursor.execute("select * from songs where karma>? and time < ?",
     (rand, int(time.time()-(60*(flood_delay-trigger*3)))))
   data = cursor.fetchall()
   if data == []:
+    updateone()
     addsong()
   else:
     songdata = random.choice(data)
@@ -133,49 +134,51 @@ if password:
     log("Wrong password?", stdout=True)
     exit(2)
 
+log("Connected")
 
-def updatedb(sleep=0.003):
-  os.nice(20)
-  log("Updating database...")
-  db = sqlite3.connect(os.path.expanduser(dbfile))
-  cursor = db.cursor()
-  cursor.execute("create table if not exists songs(\
-    file text, listened int, added int, karma real, time int\
-    );")
-  stale = []
-  for song in client.list("file"):
-    song = unicode(song, enc)
-    cursor.execute("select * from songs where file=?", (song,))
-    if cursor.fetchone() == None:
-      cursor.execute("insert into songs values (?, 0, 0, 0.5, 0)",
-        (song,))
-    time.sleep(sleep)
-  db.commit()
-  for song in cursor.execute("select file from songs"):
-    if not os.path.isfile("/home/codl/music/" + song[0]):
-      stale.append(song[0])
-      time.sleep(sleep)
-  for song in stale:
+allsongs = []
+def updateone():
+  os.nice(5)
+  if allsongs == []:
+    cursor.execute("create table if not exists songs(\
+      file text, listened int, added int, karma real, time int\
+      );")
+    db.commit()
+    for song in client.list("file"):
+      allsongs.append(unicode(song, enc))
+    for song in cursor.execute("select file from songs;"):
+      allsongs.append(song[0])
+    random.shuffle(allsongs)
+
+  song = allsongs.pop()
+  #for j in range(i) : allsongs.pop() #Remove duplicates
+  # Verify in DB
+  cursor.execute("select * from songs where file=?", (song,))
+  if cursor.fetchone() == None:
+    cursor.execute("insert into songs values (?, 0, 0, 1, 0)",
+      (song,))
+    db.commit()
+
+  # Verify in fs
+  if not os.path.isfile("/home/codl/music/" + song):
     cursor.execute("delete from songs where file=?", (song,))
-    time.sleep(sleep)
-  db.commit()
-  log("Database updated! :)")
 
-time.sleep(3)
-updater = threading.Thread(target=updatedb)
-updater.start()
-updater.join(7)
-if updater.isAlive():
-  log("Update is too slow, starting with incomplete db")
-  log("The db will continue updating in the background")
+  os.nice(-5)
 
 
 db = sqlite3.connect(os.path.expanduser(dbfile))
 cursor = db.cursor()
 
+for i in range(30):
+  updateone()
+
 armed = 1
 
+log("Ready")
+
 while __name__ == "__main__":
+  updateone()
+
   if client.status()["consume"] == "0":
     cursongid = client.status()["songid"]
     for song in client.playlistid():
@@ -185,7 +188,7 @@ while __name__ == "__main__":
   else:
     plistlength = trigger
 
-  while len(client.playlist()) < plistlength:
+  if len(client.playlist()) < plistlength:
     addsong()
   if client.status()['state'] == "play":
     times = client.status()['time'].split(":")
